6a7e2922c2d50ad5919ef6ae77e78f87ad562149
Gabriel <gabriel.moreira.alvarenga@gmail.com>
2022-03-31 18:39:08 -0300

    Add controllers

---
+++ src/Upstart13.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Upstart13.Api.Controllers
+{
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
+    }
+}
--- src/Upstart13.Api/Controllers/WeatherForecastController.cs
+++ src/Upstart13.Api/Controllers/WeatherForecastController.cs
@@ -1,16 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+using Upstart13.Application.Queries.WeatherForecast;
 
 namespace Upstart13.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : BaseApiController
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,16 +14,16 @@ namespace Upstart13.Api.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string street, [FromQuery] string city, [FromQuery] string state, [FromQuery] string zip)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Ok(await Mediator.Send(new WeatherForecastQuery
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Street = street,
+                City = city,
+                State = state,
+                Zip = zip
+            }));
         }
     }
 }
