4564cf66a2622bf39a845850206ecdf89d1ea71b
Gabriel <gabriel.moreira.alvarenga@gmail.com>
2022-03-31 18:38:35 -0300

    Add viewModel

---
+++ src/Upstart13.Application/ViewModels/WeatherForecastResponseViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Upstart13.Application.ViewModels
+{
+    public class WeatherForecastResponseViewModel
+    {
+        public IEnumerable<PeriodModel> Periods { get; set; }
+    }
+    public class PeriodModel
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string IsDaytime { get; set; }
+        public string Temperature { get; set; }
+        public string TemperatureUnit { get; set; }
+        public string TemperatureTrend { get; set; }
+        public string WindSpeed { get; set; }
+        public string WindDirection { get; set; }
+        public string Icon { get; set; }
+        public string ShortForecast { get; set; }
+        public string DetailedForecast { get; set; }
+    }
+}
