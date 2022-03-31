4dc6ceee0c6da011a260bb9021aa032301910528
Gabriel <gabriel.moreira.alvarenga@gmail.com>
2022-03-31 18:38:19 -0300

    Add validations

---
+++ src/Upstart13.Application/Validations/WeatherForecastQueryValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Upstart13.Application.Queries.WeatherForecast;
+
+namespace Upstart13.Application.Validations
+{
+    public class WeatherForecastQueryValidator : AbstractValidator<WeatherForecastQuery>
+    {
+        public WeatherForecastQueryValidator()
+        {
+            RuleFor(a => a.Street)
+                .NotEmpty()
+                .WithMessage("Street is required")
+                .MaximumLength(200)
+                .WithMessage("The Street max length is 200 characters");
+
+            RuleFor(a => a.City)
+                .NotEmpty()
+                .WithMessage("City is required")
+                .MaximumLength(200)
+                .WithMessage("The City max length is 200 characters");
+
+            When(t => string.IsNullOrEmpty(t.State), () =>
+            {
+                RuleFor(t => t.Zip)
+                    .NotEmpty()
+                    .WithMessage("Specify street with city and state or zip");
+            });
+        }
+    }
+}
