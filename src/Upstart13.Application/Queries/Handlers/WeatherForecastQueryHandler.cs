ee9cb97ecb0548afaee098db14e2096418d73f7b
Gabriel <gabriel.moreira.alvarenga@gmail.com>
2022-03-31 18:38:08 -0300

    Add queries

---
+++ src/Upstart13.Application/Queries/Handlers/WeatherForecastQueryHandler.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using MediatR;
+using Upstart13.Application.Notifications;
+using Upstart13.Application.Queries.WeatherForecast;
+using Upstart13.Application.Validations;
+using Upstart13.Application.ViewModels;
+using Upstart13.Infrastructure.ExternalCommunication.Interfaces;
+
+namespace Upstart13.Application.Queries.Handlers
+{
+    public class WeatherForecastQueryHandler :
+        IRequestHandler<WeatherForecastQuery, WeatherForecastResponseViewModel>
+    {
+
+        private readonly IWeatherService weatherService;
+        private readonly NotificationContext notification;
+        private readonly IMapper mapper;
+
+        public WeatherForecastQueryHandler(IWeatherService _weatherService,
+            IMapper _mapper,
+            NotificationContext _notification)
+        {
+            weatherService = _weatherService;
+            mapper = _mapper;
+            notification = _notification;
+        }
+
+        public async Task<WeatherForecastResponseViewModel> Handle(WeatherForecastQuery request, CancellationToken cancellationToken)
+        {
+            var validationResult = new WeatherForecastQueryValidator().Validate(request);
+            if (!validationResult.IsValid)
+            {
+                notification.AddNotifications(validationResult);
+                return null;
+            }
+
+            var convertedAddress = await weatherService.QueryGeocodding(request.GetUrl());
+            if (convertedAddress == null || 
+                convertedAddress.Result == null || 
+                convertedAddress.Result.AddressMatches == null ||
+                convertedAddress.Result.AddressMatches.Coordinates == null)
+            {
+                notification.AddNotification("AddressError", "Address not found!");
+                return null;
+            }
+
+            var weatherInfo = await weatherService.QueryWeather(convertedAddress.Result.AddressMatches.Coordinates.X, convertedAddress.Result.AddressMatches.Coordinates.Y);
+
+            if (weatherInfo == null ||
+                weatherInfo.Properties == null ||
+                weatherInfo.Properties.Forecast == null)
+            {
+                notification.AddNotification("WeatherError", "Weather not found!");
+                return null;
+            }
+
+            var weatherForecast = await weatherService.QueryWeatherForecast(weatherInfo.Properties.Forecast);
+
+            if (weatherForecast == null ||
+                weatherForecast.Properties == null ||
+                weatherForecast.Properties.Periods == null)
+            {
+                notification.AddNotification("WeatherForecastError", "Weather Forecast not found!");
+                return null;
+            }
+
+            return mapper.Map<WeatherForecastResponseViewModel>(weatherForecast);
+        }
+    }
+}
---
+++ src/Upstart13.Application/Queries/WeatherForecast/WeatherForecastQuery.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Upstart13.Application.ViewModels;
+
+namespace Upstart13.Application.Queries.WeatherForecast
+{
+    public class WeatherForecastQuery : IRequest<WeatherForecastResponseViewModel>
+    {
+        public string Street { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+
+        public string GetUrl()
+        {
+            var result = $"street={Street.Replace(" ", "+").Replace(",", "%2C")}";
+            result += string.IsNullOrEmpty(City) ? $"&city={City.Replace(" ", "+").Replace(",", "%2C")}" : string.Empty;
+            result += string.IsNullOrEmpty(State) ? $"&state={State.Replace(" ", "+").Replace(",", "%2C")}" : string.Empty;
+            result += string.IsNullOrEmpty(Zip) ? $"&zip={Zip.Replace(" ", "+").Replace(",", "%2C")}" : string.Empty;
+            return result;
+        }
+    }
+}
