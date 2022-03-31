9def20e5a1c3c22750590722f0407128fd8b7fb3
Gabriel <gabriel.moreira.alvarenga@gmail.com>
2022-03-31 18:38:56 -0300

    Add filters

---
+++ src/Upstart13.Api/Filters/ApiExceptionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace Upstart13.Api.Filters
+{
+    public class ApiExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            HttpResponse response = context.HttpContext.Response;
+
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            response.ContentType = "application/json";
+            context.Result = new JsonResult($"An error occurred while processing the request {context.Exception} \n {context.Exception.InnerException}");
+        }
+    }
+}
